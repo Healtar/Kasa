caff8a3f53c211d48dc45fd3ddaf396ea3259acb
dzerahovic <pierre.dzerahovic@gmail.com>
2023-01-25 18:40:56 +0100

    structure

--- src/pages/FicheLogement/FicheLogement.js
+++ src/pages/FicheLogement/FicheLogement.js
@@ -1,18 +1,22 @@
 // import React from "react";
 import logements from '../../data/logements.json';
 import Tag from '../../components/Tag/Tag'
-import { useParams } from "react-router-dom";
 import Dropdown from '../../components/Dropdown/Dropdown';
+import Carousel from '../../components/Carousel/Carousel';
+import { useParams } from "react-router-dom";
+
 
 
 
 export default function FicheLogement() {
+
     const params = useParams();
     const id = params.id
     const logement = logements.find((logement) => logement.id === id);
+    
     return(
         <main className="k-fiche-logement container">
-            {/* <img src={}/> */}
+            {/* <img src={}/> */}<Carousel/>
             <h2>{logement.title}</h2>
             <p>{logement.location}</p>
             <ul>
