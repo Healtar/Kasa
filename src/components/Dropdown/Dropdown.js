625fa439f3e3238f471f4cba66b11b17b5c892f8
Healtar <pierre.dzerahovic@gmail.com>
2023-01-25 22:12:45 +0100

    collapse

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,22 +1,58 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import styled from 'styled-components';
 import './Dropdown.scss';
 
 export default function Dropdown({title, description}) {
     
     const [isOpen, setIsOpen] = useState(false);
+    const [contentHeight, setContentHeight] = useState(0);
+    const [styledContent, setStyledContent] = useState()
 
 
-    const handleOpen = () =>{
+    const content = document.getElementById('content');
+    const StyledContent = styled.content`
+                            maxHeight: ${contentHeight};
+                            padding: 0px;
+                        `;
+
+    const handleOpen = () => {
         setIsOpen(!isOpen)
-        console.log(isOpen);
-    }
 
+    }
+     useEffect(() => {
+        if(isOpen)
+        {
+            const content = document.getElementById('content');
+            const size = content.scrollHeight + 100;
+            setContentHeight(size);
+            StyledContent = styled.content`
+                                maxHeight: ${contentHeight};
+                                padding: 50px;
+                            `;
+}
+        else
+        {
+            const content = document.getElementById('content');
+            content.className = "offset offset__close";
+            setContentHeight(0);
+            StyledContent = styled.content`
+                                maxHeight: ${contentHeight};
+                                padding: 0px;
+                            `;
+        }
+    
+ },[isOpen])
     return(
+
        <div onClick={() => handleOpen()} className="dropdown-container">
-        <h3>{title}</h3>
-        {isOpen === true ? <div className="offset offset__open">{description}</div> : <div className="offset offset__close">{description}</div>}
+        <h3>{title} {isOpen ? 'Open' : 'Close'}</h3>
+        <StyledContent id="content" className="offset offset__open" style={{ maxHeight: `calc(${contentHeight}px)`,}}>{description}</StyledContent>
+        {/* {isOpen === true ? <div className="offset offset__open">{description}</div> : <div className="offset offset__close">{description}</div>} */}
         </div> 
-        
+   
+   
+    
+    
         
     
     )
